29d9e5cc6fe0565d02ac512a9f45da30be7bbc13
ubuntu <ubuntu@aaryan.>
2025-08-31 14:42:57 +0000

    changed src files to handle continuous communication (need to optimise more)

--- src/client.cpp
+++ src/client.cpp
@@ -1,19 +1,65 @@
-#include <iostream>
 #include "client.hpp"
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+#include <limits>
+
+TcpClient* clientPtr = nullptr; // global pointer for signal handler
+
+void handleSigint(int signum) {
+    std::cout << "\nCaught Ctrl+C, closing connection...\n";
+    if(clientPtr) delete clientPtr; // destructor will close socket
+    exit(signum);
+}
 
 int main() {
+    signal(SIGINT, handleSigint);
+
     try {
-        std::cout<< "Enter server ip address: ";
-        std::string ipa;
-        std::cin >> ipa;
-        TcpClient client(ipa, "53490");
-        client.sendData("helloworld");
-        std::string reply = client.receiveData();
-        if(!reply.empty()) {
+        std::cout << "Enter server IP address: ";
+        std::string serverIp;
+        std::cin >> serverIp;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        TcpClient client(serverIp, "53490");
+        clientPtr = &client;
+
+        std::cout << "Connected to server. Start chatting!\n";
+
+        while (true) {
+            std::string message;
+            std::cout << "You: ";
+            std::getline(std::cin, message);
+
+            client.sendData(message);
+
+            std::string reply = client.receiveData();
+            if(reply.empty()) {
+                std::cout << "Server disconnected.\n";
+                break;
+            }
             std::cout << "Server: " << reply << "\n";
         }
+
     } catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
     }
+
+    std::cout << "Client exited cleanly.\n";
 }
 
+// int main() {
+//     try {
+//         std::cout<< "Enter server ip address: ";
+//         std::string ipa;
+//         std::cin >> ipa;
+//         TcpClient client(ipa, "53490");
+//         client.sendData("helloworld");
+//         std::string reply = client.receiveData();
+//         if(!reply.empty()) {
+//             std::cout << "Server: " << reply << "\n";
+//         }
+//     } catch (const std::exception& e) {
+//         std::cerr << e.what() << "\n";
+//     }
+// }
--- src/server.cpp
+++ src/server.cpp
@@ -1,22 +1,65 @@
-#include <iostream>
 #include "server.hpp"
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+TcpServer* serverPtr = nullptr; // global pointer to access the server in the signal handler
+
+void handleSigint(int signum) {
+    std::cout << "\nCaught Ctrl+C, shutting down server...\n";
+    if(serverPtr) delete serverPtr; // destructor will close sockets
+    exit(signum);
+}
 
 int main() {
+    signal(SIGINT, handleSigint); // register Ctrl+C handler
+
     try {
         TcpServer server("53490");
+        serverPtr = &server;
+
         server.acceptClient();
+        std::cout << "Client connected. Ready to chat!\n";
+
         while (true) {
             std::string msg = server.receiveData();
-        if(msg.empty()) {
-            std::cout << "Client disconnected\n";
-            break;
-        }
-        std::cout << "Client: " << msg << "\n";
-        server.sendData("Hello from server!");
-}
+            if(msg.empty()) {
+                std::cout << "Client disconnected.\n";
+                break;
+            }
+            std::cout << "Client: " << msg << "\n";
 
-    }
-    catch (const std::exception& e) {
+            std::string reply;
+            std::cout << "Enter reply: ";
+            std::getline(std::cin, reply);
+
+            server.sendData(reply);
+        }
+
+    } catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
     }
+
+    std::cout << "Server exited cleanly.\n";
 }
+
+
+// int main() {
+//     try {
+//         TcpServer server("53490");
+//         server.acceptClient();
+//         while (true) {
+//             std::string msg = server.receiveData();
+//         if(msg.empty()) {
+//             std::cout << "Client disconnected\n";
+//             break;
+//         }
+//         std::cout << "Client: " << msg << "\n";
+//         server.sendData("Hello from server!");
+// }
+
+//     }
+//     catch (const std::exception& e) {
+//         std::cerr << e.what() << "\n";
+//     }
+// }
