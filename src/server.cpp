2ca0f3ea0e6076a61cc83b28d9401aad45a79924
ubuntu <ubuntu@aaryan.>
2025-08-31 14:23:23 +0000

    added cout statements in client and server src files

--- src/server.cpp
+++ src/server.cpp
@@ -5,8 +5,16 @@ int main() {
     try {
         TcpServer server("53490");
         server.acceptClient();
-        server.sendData("Hello from server!");   // send first
-        server.receiveData();
+        while (true) {
+            std::string msg = server.receiveData();
+        if(msg.empty()) {
+            std::cout << "Client disconnected\n";
+            break;
+        }
+        std::cout << "Client: " << msg << "\n";
+        server.sendData("Hello from server!");
+}
+
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
