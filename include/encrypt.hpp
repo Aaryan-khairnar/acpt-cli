2af110c2eaf6db5a9175457eb8acbf3a3eafe965
ubuntu <ubuntu@aaryan.>
2025-09-03 10:29:23 +0000

    added check prime function

--- include/encrypt.hpp
+++ include/encrypt.hpp
@@ -6,10 +6,24 @@
 #include <vector>
 #include <string>
 #include <stdexcept>
+#include <math.h>
 
 class Encrypt{
+long long p = 0;
+long long q = 0;
 
 public:
+
+int checkprime(long int pr){
+    int i;
+    long int j = sqrt(pr);
+    for (i = 2; i <= j; i++){
+        if (pr % i == 0) return 0;
+    }
+    return 1;
+}
+
+
 
 };
 
